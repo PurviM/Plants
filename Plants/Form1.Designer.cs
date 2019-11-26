8311bd7b23cda275d439b4be7f41a597919ebd8f
Purvi Patel <ppatel@careeredge.ca>
2019-11-26 11:06:29 -0500

    WaterMango Project Commit

---
+++ Plants/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Plants
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnP1 = new System.Windows.Forms.Button();
+            this.btnP2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnP4 = new System.Windows.Forms.Button();
+            this.btnP3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnP5 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.btnS1 = new System.Windows.Forms.Button();
+            this.btnS2 = new System.Windows.Forms.Button();
+            this.btnS3 = new System.Windows.Forms.Button();
+            this.btnS4 = new System.Windows.Forms.Button();
+            this.btnS5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnP1
+            // 
+            this.btnP1.Location = new System.Drawing.Point(129, 41);
+            this.btnP1.Name = "btnP1";
+            this.btnP1.Size = new System.Drawing.Size(165, 28);
+            this.btnP1.TabIndex = 0;
+            this.btnP1.Text = "Start";
+            this.btnP1.UseVisualStyleBackColor = true;
+            this.btnP1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnP2
+            // 
+            this.btnP2.Location = new System.Drawing.Point(129, 91);
+            this.btnP2.Name = "btnP2";
+            this.btnP2.Size = new System.Drawing.Size(165, 28);
+            this.btnP2.TabIndex = 1;
+            this.btnP2.Text = "Start";
+            this.btnP2.UseVisualStyleBackColor = true;
+            this.btnP2.Click += new System.EventHandler(this.btnP2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Plant1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Plant2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(557, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Plant4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(57, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Plant3";
+            // 
+            // btnP4
+            // 
+            this.btnP4.Location = new System.Drawing.Point(129, 184);
+            this.btnP4.Name = "btnP4";
+            this.btnP4.Size = new System.Drawing.Size(165, 28);
+            this.btnP4.TabIndex = 6;
+            this.btnP4.Text = "Start";
+            this.btnP4.UseVisualStyleBackColor = true;
+            this.btnP4.Click += new System.EventHandler(this.btnP4_Click);
+            // 
+            // btnP3
+            // 
+            this.btnP3.Location = new System.Drawing.Point(129, 134);
+            this.btnP3.Name = "btnP3";
+            this.btnP3.Size = new System.Drawing.Size(165, 28);
+            this.btnP3.TabIndex = 5;
+            this.btnP3.Text = "Start";
+            this.btnP3.UseVisualStyleBackColor = true;
+            this.btnP3.Click += new System.EventHandler(this.btnP3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(57, 236);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Plant5";
+            // 
+            // btnP5
+            // 
+            this.btnP5.Location = new System.Drawing.Point(129, 228);
+            this.btnP5.Name = "btnP5";
+            this.btnP5.Size = new System.Drawing.Size(165, 28);
+            this.btnP5.TabIndex = 9;
+            this.btnP5.Text = "Start";
+            this.btnP5.UseVisualStyleBackColor = true;
+            this.btnP5.Click += new System.EventHandler(this.btnP5_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(557, 99);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(557, 142);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(557, 192);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "0";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(557, 236);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(13, 13);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "0";
+            // 
+            // btnS1
+            // 
+            this.btnS1.Location = new System.Drawing.Point(328, 41);
+            this.btnS1.Name = "btnS1";
+            this.btnS1.Size = new System.Drawing.Size(165, 28);
+            this.btnS1.TabIndex = 15;
+            this.btnS1.Text = "Stop";
+            this.btnS1.UseVisualStyleBackColor = true;
+            this.btnS1.Click += new System.EventHandler(this.btnS1_Click);
+            // 
+            // btnS2
+            // 
+            this.btnS2.Location = new System.Drawing.Point(328, 91);
+            this.btnS2.Name = "btnS2";
+            this.btnS2.Size = new System.Drawing.Size(165, 28);
+            this.btnS2.TabIndex = 16;
+            this.btnS2.Text = "Stop";
+            this.btnS2.UseVisualStyleBackColor = true;
+            this.btnS2.Click += new System.EventHandler(this.btnS2_Click);
+            // 
+            // btnS3
+            // 
+            this.btnS3.Location = new System.Drawing.Point(328, 134);
+            this.btnS3.Name = "btnS3";
+            this.btnS3.Size = new System.Drawing.Size(165, 28);
+            this.btnS3.TabIndex = 17;
+            this.btnS3.Text = "Stop";
+            this.btnS3.UseVisualStyleBackColor = true;
+            this.btnS3.Click += new System.EventHandler(this.btnS3_Click);
+            // 
+            // btnS4
+            // 
+            this.btnS4.Location = new System.Drawing.Point(328, 184);
+            this.btnS4.Name = "btnS4";
+            this.btnS4.Size = new System.Drawing.Size(165, 28);
+            this.btnS4.TabIndex = 18;
+            this.btnS4.Text = "Stop";
+            this.btnS4.UseVisualStyleBackColor = true;
+            this.btnS4.Click += new System.EventHandler(this.btnS4_Click);
+            // 
+            // btnS5
+            // 
+            this.btnS5.Location = new System.Drawing.Point(328, 228);
+            this.btnS5.Name = "btnS5";
+            this.btnS5.Size = new System.Drawing.Size(165, 28);
+            this.btnS5.TabIndex = 19;
+            this.btnS5.Text = "Stop";
+            this.btnS5.UseVisualStyleBackColor = true;
+            this.btnS5.Click += new System.EventHandler(this.btnS5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 362);
+            this.Controls.Add(this.btnS5);
+            this.Controls.Add(this.btnS4);
+            this.Controls.Add(this.btnS3);
+            this.Controls.Add(this.btnS2);
+            this.Controls.Add(this.btnS1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnP5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnP4);
+            this.Controls.Add(this.btnP3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnP2);
+            this.Controls.Add(this.btnP1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnP1;
+        private System.Windows.Forms.Button btnP2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnP4;
+        private System.Windows.Forms.Button btnP3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnP5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Button btnS1;
+        private System.Windows.Forms.Button btnS2;
+        private System.Windows.Forms.Button btnS3;
+        private System.Windows.Forms.Button btnS4;
+        private System.Windows.Forms.Button btnS5;
+    }
+}
+
---
+++ Plants/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Plants
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int c1=10;private int c2=10;private int c3=10;private int c4=10;private int c5 = 10;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        //method that Wait for 30 sec once user watered the Plant
+        #region 
+        async void disableButtonP1()
+        {
+           btnP1.Enabled = false; btnS1.Enabled = false;
+            await Task.Delay(TimeSpan.FromSeconds(30));
+            btnP1.Enabled = true; btnS1.Enabled = true;
+        }
+        async void disableButtonP2()
+        {
+            btnP2.Enabled = false; btnS2.Enabled = false;
+            await Task.Delay(TimeSpan.FromSeconds(30));
+            btnP2.Enabled = true; btnS2.Enabled = true;
+        }
+        async void disableButtonP3()
+        {
+            btnP3.Enabled = false; btnS3.Enabled = false;
+            await Task.Delay(TimeSpan.FromSeconds(30));
+            btnP3.Enabled = true; btnS3.Enabled = true;
+        }
+        async void disableButtonP4()
+        {
+            btnP4.Enabled = false; btnS4.Enabled = false;
+            await Task.Delay(TimeSpan.FromSeconds(30));
+            btnP4.Enabled = true; btnS4.Enabled = true;
+        }
+        async void disableButtonP5()
+        {
+            btnP5.Enabled = false; btnS5.Enabled = false;
+            await Task.Delay(TimeSpan.FromSeconds(30));
+            btnP5.Enabled = true; btnS5.Enabled = true;
+        }
+        #endregion
+
+        // Timer Tick event on Button click
+        #region
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            c1--; 
+            if (c1 == 0)
+            {
+                timer1.Stop();
+                btnP1.Text = "Start";               
+                btnP1.Enabled = true;             
+                c1=10;
+                if (timer1.Enabled == false)
+                { disableButtonP1(); }
+            }        
+            label3.Text = c1.ToString();          
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            c2--;
+            if ( c2 == 0)
+            {
+                timer2.Stop();         
+                btnP2.Text = "Start";   
+                btnP2.Enabled = true;
+                c2 = 10;
+                if (timer1.Enabled == false)
+                { disableButtonP2(); }
+            }
+           label7.Text = c2.ToString();            
+        }
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            c3--;
+            if (c3 == 0)
+            {
+                timer3.Stop();
+                btnP3.Text = "Start";
+                btnP3.Enabled = true;
+                c3 = 10;
+                if (timer1.Enabled == false)
+                { disableButtonP3(); }
+            }
+            label8.Text = c3.ToString();
+        }
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            c4--;
+            if (c4 == 0)
+            {
+                timer4.Stop();
+                btnP4.Text = "Start";
+                btnP4.Enabled = true;
+                c4 = 10;
+                if (timer1.Enabled == false)
+                { disableButtonP4(); }
+            }
+            label9.Text = c4.ToString();
+        }
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            c5--;
+            if (c5 == 0)
+            {
+                timer5.Stop();
+                btnP5.Text = "Start";
+                btnP5.Enabled = true;
+                c5 = 10;
+                if (timer1.Enabled == false)
+                { disableButtonP5(); }
+            }
+            label10.Text = c5.ToString();
+        }
+        #endregion
+
+        // Start button Click
+        #region
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Tick += new EventHandler(timer1_Tick); /* Adds the event and the event handler for the method that will process the timer event to the timer. */
+            timer1.Interval = 1000; // 1 second
+            timer1.Start();
+            btnP1.Text = "Watering Plants...";
+            btnP1.Enabled = false;
+            label3.Text = c1.ToString();         
+         }
+        private void btnP2_Click(object sender, EventArgs e)
+        {
+            timer2 = new System.Windows.Forms.Timer();
+            timer2.Tick += new EventHandler(timer2_Tick);  /* Adds the event and the event handler for the method that will process the timer event to the timer. */
+            timer2.Interval = 1000; // 1 second
+            timer2.Start();
+            btnP2.Text = "Watering Plants...";
+            btnP2.Enabled = false;
+            label7.Text = c2.ToString(); 
+        }
+        private void btnP3_Click(object sender, EventArgs e)
+        {
+            timer3 = new System.Windows.Forms.Timer();
+            timer3.Tick += new EventHandler(timer3_Tick);  /* Adds the event and the event handler for the method that will process the timer event to the timer. */
+            timer3.Interval = 1000; // 1 second
+            timer3.Start();
+            btnP3.Text = "Watering Plants...";
+            btnP3.Enabled = false;
+            label8.Text = c3.ToString(); 
+        }
+        private void btnP4_Click(object sender, EventArgs e)
+        {
+            timer4 = new System.Windows.Forms.Timer();
+            timer4.Tick += new EventHandler(timer4_Tick);  /* Adds the event and the event handler for the method that will process the timer event to the timer. */
+            timer4.Interval = 1000; // 1 second
+            timer4.Start();
+            btnP4.Text = "Watering Plants...";
+            btnP4.Enabled = false;
+            label9.Text = c4.ToString(); 
+        }
+        private void btnP5_Click(object sender, EventArgs e)
+        {
+            timer5 = new System.Windows.Forms.Timer();
+            timer5.Tick += new EventHandler(timer5_Tick);  /* Adds the event and the event handler for the method that will process the timer event to the timer. */
+            timer5.Interval = 1000; // 1 second
+            timer5.Start();
+            btnP5.Text = "Watering Plants...";
+            btnP5.Enabled = false;
+            label10.Text = c5.ToString(); 
+        }
+        #endregion
+
+        // Stop Button Click
+        #region
+        private void btnS1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            btnP1.Enabled = true; btnP1.Text = "Start";
+        }
+        private void btnS2_Click(object sender, EventArgs e)
+        {
+            timer2.Stop();
+            btnP2.Enabled = true; btnP2.Text = "Start";
+        }
+        private void btnS3_Click(object sender, EventArgs e)
+        {
+            timer3.Stop();
+            btnP3.Enabled = true; btnP3.Text = "Start";
+        }
+        private void btnS4_Click(object sender, EventArgs e)
+        {
+            timer4.Stop();
+            btnP4.Enabled = true; btnP4.Text = "Start";
+        }
+        private void btnS5_Click(object sender, EventArgs e)
+        {
+            timer5.Stop();
+            btnP5.Enabled = true; btnP5.Text = "Start";
+        }
+        #endregion
+    }
+}
